a9c769b0269049edf0b30dde4946c0374ab18a2f
Brendon Bown <brendon.bown@gmail.com>
2021-12-04 23:39:36 -0700

    Read in code file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use std::fs;
+use std::io;
+
+use structopt::StructOpt;
+
+fn main() -> io::Result<()> {
+    let config = Config::from_args();
+
+    let code = &fs::read(config.code_file)?;
+    let code = String::from_utf8_lossy(code);
+
+    println!("{}", code);
+    Ok(())
+}
+
+#[derive(StructOpt, Debug)]
+struct Config {
+    #[structopt(parse(from_os_str))]
+    code_file: PathBuf,
 }
